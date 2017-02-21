68c7477b613f6937ccb11cf1d14a881a044abf4b
Travis Hoppe <travis.hoppe@gmail.com>
2017-02-21 00:30:37 -0500

    Add coverage option

--- fabfile.py
+++ fabfile.py
@@ -4,8 +4,13 @@ def test():
     local("nosetests -vs")
     local("flake8 --ignore=E501 miniprez tests")
     local("aspell check README.md")
+    
     #local("detox")
 
 def pep():
     local("autopep8 miniprez/*.py tests/*.py -a --in-place --jobs=0")
+
+def coverage():
+    local("nosetests --with-coverage --cover-package=miniprez")
+
 

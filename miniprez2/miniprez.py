210c273c8df45f021c687190aab8c229d1eb8404
Travis Hoppe <travis.hoppe@gmail.com>
2019-03-27 23:04:58 -0400

    Update requirements and event loop

--- miniprez2/miniprez.py
+++ miniprez2/miniprez.py
@@ -1,26 +1,40 @@
+"""Miniprez
+
+Usage:
+  miniprez.py <markdown_file>
+
+Options:
+  -h --help     Show this screen.
+"""
+
 import asyncio
 import os
-
-#_current_dir = os.path.join(os.getcwd(), '*.txt')
-#target_file = os.path.abspath(__file__)
-
-target_file = __file__
+from docopt import docopt
 
 async def file_watcher(target_file, sleep_time=0.5):
+    '''
+    Watchs a file. If modified return the time since last modification.
+    '''
     
     latest_modification_time = os.path.getmtime(target_file)
     
     while True:
         current_time = os.path.getmtime(target_file)
         if current_time > latest_modification_time:
+            delta = current_time - latest_modification_time
             latest_modification_time = current_time
-            yield current_time
+            yield delta
+
         await asyncio.sleep(sleep_time)
-
-
+        
 async def main():
-    async for changes in file_watcher(target_file):
+    async for changes in file_watcher(f_markdown):
         print(changes)
 
-loop = asyncio.get_event_loop()
-loop.run_until_complete(main())
+
+if __name__ == "__main__":
+    arguments = docopt(__doc__, version='UNVERSIONED')
+    f_markdown = arguments["<markdown_file>"]
+
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())

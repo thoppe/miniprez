ffc03c0a707e1b2fec7d8d4f189580ba89a81d98
thoppe <travis.hoppe@gmail.com>
2019-03-31 12:08:15 -0400

    Stupid paragraph tags

--- custom_parser.py
+++ custom_parser.py
@@ -1,38 +1,59 @@
 import copy, re
 from mistune import Renderer, InlineGrammar, InlineLexer, Markdown
+#from mistune import RBlockGrammar, BlockLexer, Markdown
 
     
 def get_classnames(class_string):
     s = " ".join(class_string.lstrip(".").split("."))
     return s if s else None
 
-class OpenBlockClassRenderer(Renderer):
+class DivClassRenderer(Renderer):
+    
     def OpenBlockClass(self, names):
         return f"<div class='{names}'>"
 
-class OpenBlockClassInlineLexer(InlineLexer):
-    name = "OpenBlockClass"
+    def CloseBlockClass(self):
+        return f"</div>"
 
-    def enable(self):
+class DivClassInlineLexer(InlineLexer):
+
+    def enable_OpenBlockClass(self):
         # Matching pattern, two dots then valid class names dot separated
         grammar = r"\.\.[\-\w\d]+[\.[\-\w\d]+]?"
 
-        setattr(self.rules, self.name, re.compile(grammar)) 
-        self.default_rules.insert(10, self.name)
+        self.rules.OpenBlockClass = re.compile(grammar) 
+        self.default_rules.insert(10, "OpenBlockClass")
+
+    
+    def enable_CloseBlockClass(self):
+        # Matching pattern, two dots and a space
+        grammar = r"\.\."
+
+        self.rules.CloseBlockClass = re.compile(grammar) 
+        self.default_rules.insert(11, "CloseBlockClass")
 
     def output_OpenBlockClass(self, m):
         tags = get_classnames(m.group())
-        return getattr(self.renderer, self.name)(tags)
+        return self.renderer.OpenBlockClass(tags)
 
-renderer = OpenBlockClassRenderer()
-inline = OpenBlockClassInlineLexer(renderer)
-# enable the feature
-inline.enable()
+    def output_CloseBlockClass(self, m):
+        return self.renderer.CloseBlockClass()
 
-markdown = Markdown(renderer, inline=inline)
+renderer = DivClassRenderer()
+inline = DivClassInlineLexer(renderer)
+# enable the features
+inline.enable_OpenBlockClass()
+inline.enable_CloseBlockClass()
+
+markdown = Markdown(renderer, parse_inline_html=False, inline=inline)
 
 tx0 = "Here we go ..aligncenter.black fool."
-tx0 = "# ..aligncenter.black fool."
+tx0 = '''# ..aligncenter.black fool.
+..
+assaf
+
+that
+'''
 tx1 = markdown(tx0)
 
 print(tx0)

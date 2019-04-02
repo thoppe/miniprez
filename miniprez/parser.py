b38632646977c76d861abf889683b41f8b901f52
thoppe <travis.hoppe@gmail.com>
2019-04-02 14:26:56 -0400

    Pulled out CDN code

--- miniprez/parser.py
+++ miniprez/parser.py
@@ -17,6 +17,24 @@ logger = logging.getLogger("miniprez")
 # _tag_pattern = re.compile(".@([a-z]+)")
 # slide_class_pattern = re.compile(r"[^\\]\.\.[\-\w\d]+[\.[\-\w\d]+]?\s")
 
+CDN_KaTeX_css = {
+    "src": "https://cdn.jsdelivr.net/npm/katex@0.10.1/dist/katex.min.css",
+    "cdn": True,
+    "integrity":
+    "sha384-dbVIfZGuN1Yq7/1Ocstc1lUEm+AT+/rCkibIcC/OmWo5f0EA48Vf8CytHzGrSwbQ",
+    "crossorigin":"anonymous"
+}
+
+CDN_KaTeX_js = {
+    "src": "https://cdn.jsdelivr.net/npm/katex@0.10.1/dist/katex.min.js",
+    "cdn": True,
+    "integrity":
+    "sha384-2BKqo+exmr9su6dir+qCw08N2ZKRucY4PrGQPPWU1A7FtlCGjmEGFqXCv5nyM5Ij",
+    "crossorigin":"anonymous",
+    #defer=None,
+}
+
+
 
 def slide_parser(html):
     """
@@ -103,23 +121,8 @@ def build_body(html):
     ):
         logger.warning("EQUATION DETECTED. Currently using CDN.")
 
-        add_script(
-            soup,
-            "https://cdn.jsdelivr.net/npm/katex@0.10.1/dist/katex.min.js",
-            cdn=True,
-            integrity="sha384-2BKqo+exmr9su6dir+qCw08N2ZKRucY4PrGQPPWU1A7FtlCGjmEGFqXCv5nyM5Ij",
-            crossorigin="anonymous",
-            #defer=None,
-        )
-
-        add_css(
-            soup,
-            "https://cdn.jsdelivr.net/npm/katex@0.10.1/dist/katex.min.css",
-            cdn=True,
-            integrity="sha384-dbVIfZGuN1Yq7/1Ocstc1lUEm+AT+/rCkibIcC/OmWo5f0EA48Vf8CytHzGrSwbQ",
-            crossorigin="anonymous"
-        )
-
+        add_script(soup, **CDN_KaTeX_js)
+        add_css(soup, **CDN_KaTeX_css)
         add_script(soup, "static/js/render_equations.js")
 
     # Add the HTML doctype
